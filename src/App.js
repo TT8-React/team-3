e4c02dd373b8ebe4a83d4e1cb63270c7edfbfd14
yahya sh <yahya240_1@hotmail.com>
2023-02-14 18:14:28 +0200

    creating project pages and routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import {Routes, Route} from 'react-router-dom';
 import styled from 'styled-components'
 
+//themes and global styles 
 import { ThemeProvider } from 'styled-components';
 import { defaultTheme } from './global/themes';
 import Global from './global/global';
+
+// routes
+import MainRoutes from './routes';
 
 function App() {
   return (
@@ -10,6 +15,9 @@ function App() {
       <ThemeProvider theme={defaultTheme}>
         <Global />
         <Test>Team 3</Test>
+        <Routes>
+          <Route path="/*" element={<MainRoutes />} />
+        </Routes>
       </ThemeProvider>
     </>
   );
