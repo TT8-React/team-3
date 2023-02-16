864d8243ddd08054590542f81721501c6e315c2f
abd qaddora <57998399+AbdQaddora@users.noreply.github.com>
2023-02-16 18:22:18 +0200

    finish upload input

--- src/components/UploadInput/index.jsx
+++ src/components/UploadInput/index.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useId } from 'react'
 import Style from './style'
 
-const UploadInput = () => {
+// images
+import uploadIcon from '../../assets/images/icons/upload.png'
+const UploadInput = ({ file, setFile }) => {
+    const id = useId();
+
+    const handelChange = (e) => {
+        setFile(e.target.value)
+    }
+
     return (
         <Style>
-            <input type="file" />
+            <input
+                className="upload_input"
+                type="file"
+                id={id}
+                onChange={handelChange}
+                value={file}
+            />
+            <label className="upload_label" htmlFor={id}>
+                <img src={uploadIcon} alt="upload" />
+                {file ? file : "Upload here..."}
+            </label>
         </Style>
     )
 }
