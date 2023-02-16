35b1934b699e325098f7342c5748dac00bcdf294
Hadeel-Ayman <haddelayman4@gmail.com>
2023-02-17 00:53:43 +0300

    edit avatar component

---
+++ src/components/Avatar/index..js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+const Avatar = () => {
+  return (
+    <AvatarStyle>
+      <img src="" alt="" />
+    </AvatarStyle>
+  );
+};
+
+export default Avatar;
+
+export const AvatarStyle = styled.div`
+  width: 57px;
+  height: 57px;
+  border-radius: 50%;
+`;
