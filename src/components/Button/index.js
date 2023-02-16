1b1e3489780ab0da60cbd855e9a9b73457b42319
Hadeel-Ayman <haddelayman4@gmail.com>
2023-02-17 00:18:42 +0300

    edit Button component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,9 +1,8 @@
 import styled from "styled-components";
-import { defaultTheme } from "../../global/themes";
 import React from "react";
 
 const Button = ({ title, width }) => {
-  return <ButtonStyle width={width}>{title}</ButtonStyle>
+  return <ButtonStyle width={width}>{title}</ButtonStyle>;
 };
 
 export default Button;
@@ -13,9 +12,9 @@ export const ButtonStyle = styled.button`
   height: 40px;
   border-radius: 5px;
   padding: 0 16px;
-  background-color: ${defaultTheme.pallet.dark};
-  font-size: ${defaultTheme.typography.body1};
-  color: ${defaultTheme.pallet.light};
+  background-color: ${(props) => props.theme.pallet.dark};
+  font-size: ${(props) => props.theme.typography.body1};
+  color: ${(props) => props.theme.pallet.light};
   font-weight: 700;
   cursor: pointer;
 `;
