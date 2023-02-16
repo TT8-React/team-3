d62dbf109e654e59cbc6fcbc2dd3b30e67a7d80c
Hadeel-Ayman <haddelayman4@gmail.com>
2023-02-16 23:22:35 +0300

    add Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+import { defaultTheme } from "../../global/themes";
+
+const Header = () => {
+  return <Header></Header>
+};
+
+export default Header;
+
+export const HeaderStyle = styled.div`
+  width: 1242px;
+  height: 91px;
+  background-color: ${defaultTheme.pallet.gray100};
+`;
