183e415fcc99786196e9b4402e8a0a0f6b9dbe53
Hadeel-Ayman <haddelayman4@gmail.com>
2023-02-17 00:48:13 +0300

    edit Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 const Header = () => {
-  return <Header></Header>
+  return <HeaderStyle></HeaderStyle>
 };
 
 export default Header;
