164926d10ff39386319412daf34eb29712ca8318
abd qaddora <57998399+AbdQaddora@users.noreply.github.com>
2023-02-16 19:29:06 +0200

    create logo component

---
+++ src/components/Logo/style.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Style = styled.div`
+    padding: 22px 32px;
+    background-color: ${props => props.theme.colors.gray300};
+`
+
+export default Style;
