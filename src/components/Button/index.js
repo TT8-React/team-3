c6f378a91a097d06ff122915478ef9cbe871690a
yahya sh <yahya240_1@hotmail.com>
2023-02-17 18:17:02 +0200

    fixing button component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,20 +1,24 @@
 import styled from "styled-components";
 import React from "react";
 
-const Button = ({ title, width }) => {
-  return <ButtonStyle width={width}>{title}</ButtonStyle>;
+const Button = ({ children}) => {
+  return <ButtonStyle>{children}</ButtonStyle>;
 };
 
 export default Button;
 
 export const ButtonStyle = styled.button`
-  width: ${(props) => props.width};
-  height: 40px;
   border-radius: 5px;
-  padding: 0 16px;
+  padding: 10px 16px;
   background-color: ${(props) => props.theme.pallet.dark};
   font-size: ${(props) => props.theme.typography.body1};
   color: ${(props) => props.theme.pallet.light};
   font-weight: 700;
   cursor: pointer;
+
+  transition: 0.3s box-shadow ease-in-out;
+  &:hover{
+    box-shadow: inset 100vw 0 0 0 ${({ theme: { colors: { primary } } }) => primary.main};
+    transition: 0.6s box-shadow ease-in-out;
+  }
 `;
