31e4c1989555e9c4fc10a7180dcee33d6ff0a7de
Ibrahim <113946679+IbrahimSahhar@users.noreply.github.com>
2023-02-20 23:23:58 +0300

    add Loading Component

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 import {
   Login,
@@ -12,40 +12,40 @@ import {
   DataUpload,
   Governance,
   Environment,
-  CompanyDetails
-} from '../pages';
-import CreateReport from '../pages/CreateReport';
-import CustomizeReport from '../pages/CustomizeReport';
-import GriReport from '../pages/GriReport';
-import MyReport from '../pages/MyReport';
-import SebiReport from '../pages/SebiReport';
+  CompanyDetails,
+} from "../pages";
+import CreateReport from "../pages/CreateReport";
+import CustomizeReport from "../pages/CustomizeReport";
+import GriReport from "../pages/GriReport";
+import MyReport from "../pages/MyReport";
+import SebiReport from "../pages/SebiReport";
 
 const Router = () => {
   return (
     <Routes>
       <Route index element={<Dashboard />} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/signup' element={<Signup />} />
-      <Route path='/report'>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/report">
         <Route index element={<Report />} />
-        <Route path='view-all-reports' element={<MyReport />} />
-        <Route path='create' element={<CreateReport />} />
-        <Route path='sebi' element={<SebiReport />} />
-        <Route path='gri' element={<GriReport />} />
-        <Route path='customize' element={<CustomizeReport />} />
+        <Route path="view-all-reports" element={<MyReport />} />
+        <Route path="create" element={<CreateReport />} />
+        <Route path="sebi" element={<SebiReport />} />
+        <Route path="gri" element={<GriReport />} />
+        <Route path="customize" element={<CustomizeReport />} />
       </Route>
-      <Route path='/dataUpload' element={<DataUpload />} />
-      <Route path='/analytics'>
+      <Route path="/dataUpload" element={<DataUpload />} />
+      <Route path="/analytics">
         <Route index element={<Analytics />} />
-        <Route path='environment' element={<Environment />} />
-        <Route path='social' element={<Social />} />
-        <Route path='governance' element={<Governance />} />
+        <Route path="environment" element={<Environment />} />
+        <Route path="social" element={<Social />} />
+        <Route path="governance" element={<Governance />} />
       </Route>
-      <Route path='/companyDetails' element={<CompanyDetails />} />
-      <Route path='*' element={<NotFound />} />
+      <Route path="/companyDetails" element={<CompanyDetails />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
-  )
-}
+  );
+};
 
 export const PATHS = {
   DASHBOARD: "/",
@@ -62,6 +62,6 @@ export const PATHS = {
   SOCIAL: "/analytics/social",
   GOVERNANCE: "/analytics/governance",
   COMPANY_DETAILS: "/companyDetails",
-}
+};
 
-export default Router
+export default Router;
