27844418164bd2f0d44545b5003fef2a2457db37
abd qaddora <57998399+AbdQaddora@users.noreply.github.com>
2023-02-21 14:22:00 +0200

    remove git keep

--- src/global/global.js
+++ src/global/global.js
@@ -5,11 +5,11 @@ const Global = createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
-        font-family: 'Inter', sans-serif;
     }
 
     body{
         background-color: ${props => props.theme.pallet.light200};
+        font-family: 'Inter', sans-serif;
     }
 
     a{
