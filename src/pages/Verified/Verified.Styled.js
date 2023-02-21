d2948e3a982ebbe6af10e99f839c2fc93e43a350
Ibrahim <113946679+IbrahimSahhar@users.noreply.github.com>
2023-02-21 12:46:40 +0300

    create Verified page

---
+++ src/pages/Verified/Verified.Styled.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const VerifiedStyled = styled.div`
+  .card {
+    width: 522px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    margin: 60px auto;
+    h1 {
+      margin: 60px 0;
+    }
+    button {
+      display: block;
+      margin: 84px auto 40px;
+    }
+  }
+`;
